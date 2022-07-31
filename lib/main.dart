03962e6c3b042e8d379c923cadceb6a4685e8cff
Javi <javivillarreal@23branches.com>
2022-07-31 16:37:59 -0700

    added category meals screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './category_meals_screen.dart';
-import './categories_screen.dart';
+import 'screens/category_meals_screen.dart';
+import 'screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
       home: CategoriesScreen(),
       initialRoute: '/', //default route
       routes: {
-        '/': (ctx) => CategoriesScreen(),
+        '/categories-screen': (ctx) => CategoriesScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
       },
     );
